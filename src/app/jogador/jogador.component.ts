2b9195d58863c51efd6825c11dab37a1b3d9b301
Paula Cavalcante <akilfreitas@gmail.com>
2018-12-01 09:04:37 -0300

    first commit

--- src/app/jogador/jogador.component.ts
+++ src/app/jogador/jogador.component.ts
@@ -10,31 +10,13 @@ import { Route, Router } from '@angular/router';
   providers: [DBService]
 })
 export class JogadorComponent implements OnInit {
-
   novoJogador: Jogador;
-
-  Jogador: Jogador[];
-
   carregando: boolean;
-
   constructor(private database: DBService, private router: Router) {
     this.novoJogador = new Jogador();
-
-    this.carregarJogador();
   }
 
   ngOnInit() {
-  }
-
-  private carregarJogador() {
-    this.carregando = true;
-
-    this.database.listar<Jogador>('Jogador')
-      .then(JogadorDB => {
-        this.Jogador = JogadorDB;
-
-        this.carregando = false;
-      });
   }
 
   cadastrar() {
@@ -43,32 +25,6 @@ export class JogadorComponent implements OnInit {
         alert('jogador cadastrado com sucesso');
         this.router.navigate(["lista-dejogadores"]);
         this.novoJogador = new Jogador();
-        this.carregarJogador();
-      });
-  }
-
-  remover(uid: string) {
-    this.database.remover('Jogador', uid)
-      .then(() => {
-        alert('jogador removido com sucesso');
-
-        this.carregarJogador();
-      });
-  }
-
-  editar(Jogador) {
-    Jogador.editando = true;
-  }
-
-  cancelEdit(Jogador) {
-    Jogador.editando = false;
-  }
-
-  confirmEdit(Jogador) {
-    this.database.atualizar('Jogador', Jogador.uid, { nome: Jogador.nome, posicao: Jogador.posicao })
-      .then(() => {
-        alert('jogador atualizado com sucesso');
-        this.carregarJogador();
       });
   }
 }
